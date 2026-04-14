c83dc0050b380a63fb8b06346ff5bf45c75f2e8d
EmanueleSca5 <emanuele.scagliarini05@gmail.com>
2026-04-14 20:09:19 +0200

    dom-es-3

---
+++ DOM-3.js
@@ -0,0 +1,66 @@
+let via = document.querySelector(".via")
+let riavvia = document.querySelector(".riavvia")
+let pausa = document.querySelector(".pausa")
+let riprendi = document.querySelector(".riprendi")
+let reset = document.querySelector(".reset")
+let input = document.querySelector("#inp")
+
+let valore = input.value
+console.log(valore);
+
+
+
+   let interval = ""
+
+function createInterval(){
+    let counter = input.value
+     interval = setInterval(()=>{
+
+        if(counter <= valore && counter > 0){
+            counter--
+            input.value = counter
+        }else{
+            input.value = "AUGURII!!!"
+            input.style.fontSize = "50px"
+            clearInterval(interval)
+        }
+    }, 1000)
+}
+
+via.addEventListener("click", ()=>{
+    if(input.value === valore){
+        createInterval()
+    }
+})
+
+
+riavvia.addEventListener("click", ()=>{
+    clearInterval(interval)
+            input.style.fontSize = "100px"
+    
+    input.value = valore
+    createInterval()
+})
+
+reset.addEventListener("click", ()=>{
+    clearInterval(interval)
+            input.style.fontSize = "100px"
+
+    input.value = valore
+})
+
+pausa.addEventListener("click", ()=>{
+    clearInterval(interval)
+            input.style.fontSize = "100px"
+
+    input.value 
+})
+
+riprendi.addEventListener("click", ()=>{
+    clearInterval(interval)
+
+    if(input.value != valore){
+
+        createInterval()
+    }
+})
